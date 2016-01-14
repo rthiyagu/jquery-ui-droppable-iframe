8ccc3fca955d833511a0edad7043b3e4b015cb77
Kevin H. Kamel <kamelkev@underprint.com>
2016-01-13 22:48:26 -0700

    Entirely rework the dropzone recalculations. The previous technique was flawed in multiple ways.
    Current implementation only handles vertical scrolling. Presumably if this passes muster horizontal
    scrolling can be accounted for as well.
    
    The current technique attempts to determine if the element is partially obscured, in which case
    special allocations are made for calculating it's offsets and proportions.
    
    If the element is fully exposed then only the offset is calculated, as the proportion will be unchanged.
    
    Note: Elements that are completely obscured are not properly handled. The proportions of such elements
    seemingly will end up becoming negative due to the partially obscured branches, preventing these
    zones from accepting a droppable when they are off the screen.

--- src/jquery-ui-droppable-iframe.js
+++ src/jquery-ui-droppable-iframe.js
@@ -1,6 +1,6 @@
 // Create new object to cache iframe offsets
 $.ui.ddmanager.frameOffsets = {};
- 
+
 // Override the native `prepareOffsets` method. This is almost
 // identical to the un-edited method, except for the last part!
 $.ui.ddmanager.prepareOffsets = function (t, event) {
@@ -8,7 +8,7 @@ $.ui.ddmanager.prepareOffsets = function (t, event) {
         m = $.ui.ddmanager.droppables[t.options.scope] || [],
         type = event ? event.type : null, // workaround for #2317
         list = (t.currentItem || t.element).find(":data(ui-droppable)").addBack(),
-        doc, frameOffset, scrollOffset, parentNodes;
+        doc, frameOffset, parentNodes;
  
     droppablesLoop: for (i = 0; i < m.length; i++) {
  
@@ -17,7 +17,7 @@ $.ui.ddmanager.prepareOffsets = function (t, event) {
             continue;
         }
  
-        // Filter out elements in the current dragoged item
+        // Filter out elements in the current dragged item
         for (j = 0; j < list.length; j++) {
             if (list[j] === m[i].element[0]) {
                 m[i].proportions().height = 0;
@@ -41,34 +41,77 @@ $.ui.ddmanager.prepareOffsets = function (t, event) {
         // Re-calculate proportions (jQuery UI ~1.10 introduced a `proportions` cache method, so support both here!)
         proportions = { width: m[i].element[0].offsetWidth, height: m[i].element[0].offsetHeight };
         typeof m[i].proportions === 'function' ? m[i].proportions(proportions) : (m[i].proportions = proportions);
-        
+
         /* ============ Here comes the fun bit! =============== */
  
         // If the element is within an another document...
         if ((doc = m[i].document[0]) !== document) {
+            var frameElement = (doc.defaultView || doc.parentWindow).frameElement; 
+
             // Determine in the frame offset using cached offset (if already calculated)
             frameOffset = $.ui.ddmanager.frameOffsets[doc];
             if (!frameOffset) {
-                // Calculate and cache the offset in our new `$.ui.ddmanager.frameOffsets` object
-                frameOffset = $.ui.ddmanager.frameOffsets[doc] = $(
-                    // Different browsers store it on different properties (IE...)
-                    (doc.defaultView || doc.parentWindow).frameElement
-                ).offset();
+                console.log('calculating, no cache found?');
+
+                // Calculate offset, different browsers store it on different properties (IE...)
+                frameOffset = $(frameElement).offset();
+
+                // cache the offset in our new `$.ui.ddmanager.frameOffsets` object
+                $.ui.ddmanager.frameOffsets[doc] = frameOffset;
             }
 
-            // prepare to determine the scroll offsets by ascending all parents and aggregating
-            scrollOffset = { left: 0, top: 0 };
-            
-            $((doc.defaultView || doc.parentWindow).frameElement).parents().each(function() {
-              console.log(this.tagName);
-              scrollOffset.left += $(this).scrollLeft();
-              scrollOffset.top += $(this).scrollTop();
-              console.log($(this).attr("id") + " " + $(this).scrollLeft() + " " + $(this).scrollTop());
-            });
 
-            // Add the frame and scroll offsets to the calculated offset
-            m[i].offset.left += frameOffset.left - scrollOffset.left;
-            m[i].offset.top += frameOffset.top - scrollOffset.top;
+            console.log('Handling element: ' + i);
+
+            var adjusted = 0;
+            if ($(frameElement).contents().scrollTop() > m[i].offset.top) {
+               console.log('---------------- Truncated Top--------------');
+               console.log('scrolled: ' + $(frameElement).contents().scrollTop());
+               console.log('frameOffset.top ' + frameOffset.top);
+               console.log('m[i].offset.top ' + m[i].offset.top);
+               console.log('proportions.height ' + proportions.height); 
+
+               proportions.height -= ($(frameElement).contents().scrollTop() - m[i].offset.top);
+               m[i].offset.top = 0;
+               console.log('modified proportions to' + proportions.height);
+               console.log('modified m[i].offset.top to' + m[i].offset.top);
+
+               adjusted = 1;
+            }
+
+            console.log(frameOffset.top + $(frameElement).height() + $(frameElement).contents().scrollTop());
+            console.log(frameOffset.top + proportions.height + m[i].offset.top);
+
+            // bottom of frame < bottom element
+            if (frameOffset.top + $(frameElement).height() + $(frameElement).contents().scrollTop() < frameOffset.top + proportions.height + m[i].offset.top) {
+               console.log('---------------- Truncated Bottom--------------');
+               console.log('scrolled: ' + $(frameElement).contents().scrollTop());
+               console.log('frameOffset.top ' + frameOffset.top);
+               console.log('m[i].offset.top ' + m[i].offset.top);
+               console.log('proportions.height ' + proportions.height);
+
+               proportions.height = (frameOffset.top + $(frameElement).height()) - (frameOffset.top + m[i].offset.top - $(frameElement).contents().scrollTop());
+               m[i].offset.top -= $(frameElement).contents().scrollTop();
+
+               console.log('modified proportions.height to' + proportions.height);
+               console.log('modified m[i].offset.top to' + m[i].offset.top);
+
+               adjusted = 1;
+            }
+
+            if (!adjusted) {
+              console.log('---------------- Fully Exposed --------------');
+              console.log('scrolled: ' + $(frameElement).contents().scrollTop());
+              console.log('frameOffset.top ' + frameOffset.top);
+              console.log('m[i].offset.top ' + m[i].offset.top);
+              console.log('proportions.height ' + proportions.height);
+
+              m[i].offset.top -= $(frameElement).contents().scrollTop();
+            }
+
+            // Add the frame offsets to the calculated offset
+            m[i].offset.left += frameOffset.left;
+            m[i].offset.top += frameOffset.top;
         }
     }
 };
